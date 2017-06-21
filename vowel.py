23535787ffc8e7427decc2ef4a14198c5b44a4d4
PrashaMeena <prashameena97@gmail.com>
2017-06-21 11:42:28 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,8 @@
 import sys
 for line in sys.stdin:
 	letter = line
-if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
-  print("Vowel")
+vowel = ['a','e','i','o','u','A','E','I','O','U']
+if letter in vowel:
+	print("Vowel")
 else:
-  print("Consonent")# your code goes here
+	print("Consonent")
