86267c71d8f71cb5d87d08915b877cc5636d46a4
PrashaMeena <prashameena97@gmail.com>
2017-06-21 11:35:48 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+import sys
+for line in sys.stdin:
+	letter = line
+if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+  print("Vowel")
+else:
+  print("Consonent")# your code goes here
